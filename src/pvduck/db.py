6280f40e149278caa5eacdbccbc39146a92e73db
Vegard Egeland <vegardegeland@gmail.com>
2024-09-28 13:20:58 +0200

    Switch to flat table with compact option
    For my use case, this is the better option. Denormalizing may be
    necessary for other use cases, but I won't support that for now.

--- src/pvduck/db.py
+++ src/pvduck/db.py
@@ -1,8 +1,10 @@
-from datetime import datetime
+import logging
 from pathlib import Path
 from typing import Optional
 
 import duckdb
+
+logger = logging.getLogger(__name__)
 
 # The log table is used to keep track of which pageviews files we have
 # already downloaded and merged into the pageviews table.
@@ -11,141 +13,49 @@ CREATE TABLE log (
     filename VARCHAR PRIMARY KEY,
     success BOOLEAN NOT NULL,
     error VARCHAR
-);
+)
 """
 
-# Denormalized domain codes with parsed data about the code components.
-DOMAIN_CODE_SQL = """
-CREATE SEQUENCE domain_code_seq START 1 MINVALUE 1 MAXVALUE 65535;
-CREATE TABLE domain_code (
-    domain_code_id USMALLINT PRIMARY KEY,
-    domain_code VARCHAR NOT NULL,
-    language VARCHAR NOT NULL,
-    domain VARCHAR NOT NULL,
-    mobile BOOLEAN NOT NULL
-);
-CREATE UNIQUE INDEX unique_domain_code ON domain_code (domain_code);
-"""
-
-# Denormalized page titles.
-PAGE_SQL = """
-CREATE SEQUENCE page_seq START 1 MINVALUE 1 MAXVALUE 4294967295;
-CREATE TABLE page (
-    page_id UINTEGER PRIMARY KEY,
-    title VARCHAR NOT NULL
-);
-CREATE UNIQUE INDEX unique_page_title ON page (title);
-"""
-
-# Fact table, aggregating views over the selected dimensions.
+# The pageviews table is simply a flat, aggregated version of the parquet
+# files. Denormalizing could benefit the table in some use cases, but for
+# the total aggregation use case I decided on, duckdb effectively stores
+# this in a flat file without much overhead. Change this if you want a
+# representation better suited for larger amounts of data.
 PAGEVIEWS_SQL = """
 CREATE TABLE pageviews (
-    domain_code_id USMALLINT NOT NULL
-        REFERENCES domain_code(domain_code_id),
-    page_id UINTEGER NOT NULL
-        REFERENCES page(page_id),
-    timestamp TIMESTAMP NOT NULL,
-    views UBIGINT NOT NULL
-);
-CREATE UNIQUE INDEX unique_pageviews
-    ON pageviews (domain_code_id, page_id, timestamp);
-"""
-
-PAGEVIEWS_FLAT_SQL = """
-CREATE TABLE pageviews_flat (
     domain_code VARCHAR NOT NULL,
     language VARCHAR NOT NULL,
     domain VARCHAR NOT NULL,
     mobile BOOLEAN NOT NULL,
     page_title VARCHAR NOT NULL,
-    timestamp TIMESTAMP NOT NULL,
-    views UBIGINT NOT NULL
-);
-CREATE UNIQUE INDEX unique_pageviews_flat
-    ON pageviews_flat (domain_code, page_title, timestamp);
-"""
-
-# Identify domain codes in the parquet file which are not already in the
-# database and insert them with an auto-incrementing ID.
-UPSERT_DOMAIN_CODE = """
-WITH unique_new_domains AS (
-  SELECT DISTINCT domain_code, language, domain, mobile
-  FROM staging_table
-),
-filtered_domains AS (
-  SELECT u.*
-  FROM unique_new_domains u
-  LEFT JOIN domain_code d
-    ON d.domain_code = u.domain_code
-  WHERE d.domain_code_id IS NULL
-),
-prepared_rows AS (
-  SELECT nextval('domain_code_seq'), *
-  FROM filtered_domains
+    views UBIGINT NOT NULL,
+    PRIMARY KEY (domain_code, page_title)
 )
-INSERT INTO domain_code
-    (domain_code_id, domain_code, language, domain, mobile)
-SELECT * FROM prepared_rows;
 """
 
-# Identify page titles in the parquet file which are not already in the
-# database and insert them with an auto-incrementing ID.
-UPSERT_PAGES = """
-WITH unique_new_pages AS (
-    SELECT DISTINCT page_title
-    FROM staging_table
-),
-filtered_pages AS (
-    SELECT u.*
-    FROM unique_new_pages u
-    LEFT JOIN page p
-        ON p.title = u.page_title
-    WHERE p.page_id IS NULL
-),
-prepared_rows AS (
-    SELECT
-        nextval('page_seq'), *
-    FROM filtered_pages
-)
-INSERT INTO page
-    (page_id, title)
-SELECT * FROM prepared_rows;
+PAGEVIEWS_INDEX_SQL = """
+CREATE UNIQUE INDEX unique_pageviews
+    ON pageviews (domain_code, page_title)
 """
 
+# Add views from a parquet file to the table, or create new rows if they
+# don't exist yet. This design is created to save space, but note that it
+# is destructive, so we need to keep track of which files we have inserted.
 UPSERT_PAGEVIEWS = """
-WITH unique_new_pageviews AS (
-    SELECT
-        d.domain_code_id, p.page_id, f.views
-    FROM
-        staging_table f
-    JOIN
-        domain_code d ON d.domain_code = f.domain_code
-    JOIN
-        page p ON p.title = f.page_title
-)
 INSERT INTO pageviews
-    (domain_code_id, page_id, timestamp, views)
+    (domain_code, language, domain, mobile, page_title, views)
 SELECT
-    domain_code_id, page_id, {timestamp} AS timestamp, views
-FROM unique_new_pageviews
-ON CONFLICT (domain_code_id, page_id, timestamp)
-    DO UPDATE SET
-        views = pageviews.views + excluded.views
-"""
-
-UPSERT_PAGEVIEWS_FLAT = """
-INSERT INTO pageviews_flat
-    (domain_code, language, domain, mobile, page_title, timestamp, views)
-SELECT
-    domain_code, language, domain, mobile, page_title, {timestamp} AS timestamp, views
-FROM staging_table
-ON CONFLICT (domain_code, page_title, timestamp)
-    DO UPDATE SET
-        views = pageviews_flat.views + excluded.views
+    domain_code, language, domain, mobile, page_title, views
+FROM
+    '{parquet}'
+ON CONFLICT
+    (domain_code, page_title)
+DO UPDATE SET
+    views = pageviews.views + excluded.views
 """
 
 
-def create_db(db: Path, flat: bool) -> None:
+def create_db(db: Path) -> None:
     """Create a new duckdb database at the specified path.
 
     Args:
@@ -159,50 +69,30 @@ def create_db(db: Path, flat: bool) -> None:
 
     # Create the database and tables
     with duckdb.connect(db) as connection:
-        if flat:
-            connection.sql(PAGEVIEWS_FLAT_SQL)
-        else:
-            connection.sql("BEGIN TRANSACTION")
-            connection.sql(LOG_SQL)
-            connection.sql(DOMAIN_CODE_SQL)
-            connection.sql(PAGE_SQL)
-            connection.sql(PAGEVIEWS_SQL)
-            connection.sql("COMMIT TRANSACTION")
+        connection.sql("BEGIN TRANSACTION")
+        connection.sql(LOG_SQL)
+        connection.sql(PAGEVIEWS_SQL)
+        connection.sql(PAGEVIEWS_INDEX_SQL)
+        connection.sql("COMMIT TRANSACTION")
 
 
-def update_db_from_parquet(
-    db: Path, parquet: Path, timestamp: datetime, flat: bool
-) -> None:
+def update_db_from_parquet(db: Path, parquet: Path) -> None:
     """Update the database with the content of the parquet file.
 
     Args:
         db (Path): The path to the database file.
         parquet (Path): The path to the parquet file.
-        timestamp (datetime): The timestamp to use for the pageviews.
 
     Raises:
         FileNotFoundError: If either file does not exist.
     """
-    if not db.exists():
+    if not db.is_file():
         raise FileNotFoundError(f"Database does not exist at {db}")
-    if not parquet.is_file() or parquet.suffix != ".parquet":
+    if not parquet.is_file():
         raise FileNotFoundError(f"Parquet file does not exist at {parquet}")
 
     with duckdb.connect(db) as connection:
-        if flat:
-            connection.sql(
-                f"CREATE TEMP TABLE staging_table AS SELECT * FROM '{parquet}'"
-            )
-            connection.sql(UPSERT_PAGEVIEWS_FLAT.format(timestamp=f"'{timestamp}'"))
-        else:
-            connection.sql("BEGIN TRANSACTION")
-            connection.sql(
-                f"CREATE TEMP TABLE staging_table AS SELECT * FROM '{parquet}'"
-            )
-            connection.sql(UPSERT_DOMAIN_CODE)
-            connection.sql(UPSERT_PAGES)
-            connection.sql(UPSERT_PAGEVIEWS.format(timestamp=f"'{timestamp}'"))
-            connection.sql("COMMIT TRANSACTION")
+        connection.sql(UPSERT_PAGEVIEWS.format(parquet=parquet))
 
 
 def update_log(
@@ -217,7 +107,80 @@ def update_log(
         error (Optional[str]): The error message if the operation failed.
     """
     with duckdb.connect(db) as connection:
-        connection.sql(
+        connection.execute(
             "INSERT INTO log (filename, success, error) VALUES (?, ?, ?)",
             (filename, success, error),
         )
+
+
+def compact_database(db: Path) -> tuple[float, float, float]:
+    """Compact the database by creating a new table and dropping the old one.
+
+    DuckDB does not automatically compact the database when upserting data,
+    which can lead to fragmentation and increased file size. This function
+    creates a new table with the same schema as the original, copies the
+    data from the original table to the new table, and then drops the
+    original table.
+
+    This requires enough disk space to create a new copy of the table, so it
+    is not run automatically.
+
+    This may be improved in the future, keep an eye on:
+    https://duckdb.org/docs/stable/operations_manual/footprint_of_duckdb/reclaiming_space.html
+
+    Args:
+        db (Path): The path to the database file.
+
+    Raises:
+        FileNotFoundError: If the database file does not exist.
+
+    Returns:
+        tuple[float, float, float]: The size of the database before and after
+            compacting, and the space saved.
+    """
+    if not db.is_file():
+        raise FileNotFoundError(f"Database does not exist at {db}")
+
+    size_pre_compacting = _file_size_mb(db)
+    logger.info("Compacting database %s", db)
+    logger.info("Size before compacting: %.2f MB", size_pre_compacting)
+
+    with duckdb.connect(db) as connection:
+        connection.sql("BEGIN TRANSACTION")
+        connection.sql(
+            """
+            CREATE TABLE pageviews_compacted AS
+            SELECT * FROM pageviews
+            """
+        )
+        connection.sql("DROP TABLE pageviews")
+        connection.sql(
+            """
+            ALTER TABLE pageviews_compacted
+            RENAME TO pageviews
+            """
+        )
+        connection.sql(PAGEVIEWS_INDEX_SQL)
+        connection.sql("COMMIT TRANSACTION")
+
+    size_post_compacting = _file_size_mb(db)
+    logger.info("Size after compacting: %.2f MB", size_post_compacting)
+    logger.info("Space saved: %.2f MB", size_pre_compacting - size_post_compacting)
+
+    return (
+        size_pre_compacting,
+        size_post_compacting,
+        size_pre_compacting - size_post_compacting,
+    )
+
+
+def _file_size_mb(path: Path) -> float:
+    """Return the size of the file in MB.
+
+    Args:
+        path (Path): The path to the file.
+
+    Returns:
+        float: The size of the file in MB.
+    """
+    return path.stat().st_size / (1024 * 1024)
